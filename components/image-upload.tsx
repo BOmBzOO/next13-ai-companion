916f7571d8f1b80507f72724b984bf024338a8d1
Bumju Ahn <bombzoo78@gmail.com>
2023-12-02 02:47:35 +0900

    Update components/image-upload.tsx

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -31,7 +31,8 @@ export const ImageUpload = ({
   return (
     <div className="space-y-4 w-full flex flex-col justify-center items-center">
       
-      <CldUploadButton options={{ maxFiles: 1 }} onUpload={(result: any) => onChange(result.info.secure_url)} uploadPreset="t4drjppf">
+      {/* <CldUploadButton options={{ maxFiles: 1 }} onUpload={(result: any) => onChange(result.info.secure_url)} uploadPreset="t4drjppf"> */}
+      <CldUploadButton options={{ maxFiles: 1 }} onUpload={(result: any) => onChange(result.info.secure_url)} uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRISET}>
         <div 
           className="
             p-4 
